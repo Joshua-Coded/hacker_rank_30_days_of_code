1b525162e78a19341fb10d9b819273f864a48e7a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-30 21:55:59 +0200

    day 1 done ☑️

---
+++ day1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+        Scanner scan = new Scanner(System.in);
+        String newText = scan.nextLine();
+        System.out.println(newText);
+    }
+}
